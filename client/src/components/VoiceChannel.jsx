137645eb8a23840694ee90119e7834b575b8a4f6
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2024-05-02 18:43:46 -0400

    remove app.css contents, add videoplayer and videoroom functionality

--- client/src/components/VoiceChannel.jsx
+++ client/src/components/VoiceChannel.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { LuPhone } from "react-icons/lu";
 import { useState } from "react";
+import {VideoRoom} from "./VideoRoom";
 
 const VoiceChannel = () => {
   const [joined, setJoined] = useState(false);
@@ -9,12 +10,12 @@ const VoiceChannel = () => {
     <div className="mt-4 mb-4">
       <div href="">
         <LuPhone className="inline-block w-6 h-6 mr-2 -mt-2" />
-        <button onClick={toggleJoin}> {joined ? "Leave Voice Channel" : "Join Voice Channel"} </button>
+        <button onClick={toggleJoin} className=""> {joined ? "Leave Voice Channel" : "Join Voice Channel"} </button>
       </div>
       <div>
         {joined && (
           <div className="mt-2">
-            <p className="text-gray-400">Voice Channel: General</p>
+            <p className="text-black">Voice Channel: General</p>
             <VideoRoom/>
           </div>
         )}
