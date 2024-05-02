05f86232cff21848367188b03948f5c71722e876
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2024-05-02 15:24:54 -0400

    sidebar stash

--- client/src/components/PythonRunner.jsx
+++ client/src/components/PythonRunner.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Editor from "@monaco-editor/react";
@@ -13,22 +13,61 @@ function PythonRunner() {
   const id = localStorage.getItem("room-ID");
   const token = localStorage.getItem("token");
 
-  const [peerID, setPeerID] = useState(null);
+  const [peerID, setPeerID] = useState("");
+  const remoteVideoRef = useRef(null);
+  const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+
+  const peerInstance = useRef(null);
 
   useEffect(() => {
     console.log("Creating peer");
     try {
       const peer = new Peer();
       console.log(peer);
-      
+
       peer.on("open", function (id) {
         console.log("My peer ID is: " + id);
         setPeerID(id);
+      }, []);
+
+      peer.on("call", (call) => {
+        //answer the call immediately
+        call.answer(window.localStream);
+        var getUserMedia =
+          navigator.getUserMedia ||
+          navigator.webkitGetUserMedia ||
+          navigator.mozGetUserMedia;
+        getUserMedia({ video: true, audio: true }, (stream) => {
+          call.answer(stream); // Answer the call with an A/V stream.
+        });
       });
+
+      peerInstance.current = peer;
     } catch (err) {
       console.log(err);
     }
   }, []);
+
+  const call = (remotePeerID) => {
+    var getUserMedia =
+      navigator.getUserMedia ||
+      navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia;
+    getUserMedia(
+      { video: true, audio: true },
+      (stream) => {
+        var call = peerInstance.current.call(remotePeerID, stream);
+        call.on("stream", (remoteStream) => {
+          // Show stream in some video/canvas element.
+          console.log("Got remote stream");
+          remoteVideoRef.current.srcObject = remoteStream;
+        });
+      },
+      function (err) {
+        console.log("Failed to get local stream", err);
+      }
+    );
+  };
 
   useEffect(() => {
     if (!token) {
@@ -96,8 +135,23 @@ function PythonRunner() {
   return (
     <div className="flex bg-gray-900 h-screen">
       <Sidebar />
+      
       <div className="flex flex-col items-center justify-center flex-grow">
         <div className="">
+          <div className="mt-24">
+            <h2 className="text-2xl">Video Test</h2>
+            <input
+              value={remotePeerIdValue}
+              onChange={(e) => setRemotePeerIdValue(e.target.value)}
+              type="text"
+              placeholder="Enter peer ID"
+              className="bg-red-500"
+            />
+            <button onClick={() => call(remotePeerIdValue)}>Call</button>
+            <div>
+              <video ref={remoteVideoRef}></video>
+            </div>
+          </div>
           <h2 className="text-2xl">Python Interpreter</h2>
           <form onSubmit={handleSubmit} className="">
             <Editor
