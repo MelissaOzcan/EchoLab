d03a15a5a860c680da55207044b827cb02f0f2e8
Nouman Syed <syednouman2114@gmail.com>
2024-05-04 20:05:07 -0400

    delete code files

--- server/utils/code-579c3563-f1e2-4926-884c-450435ebf996.py
+++
@@ -1,3 +0,0 @@
-# Hello, this is diego's test room
-
-print("Hello Gang") womp womp
--- server/utils/code-c15ceb1d-4914-4709-acc0-069186266110.py
+++
@@ -1,7 +0,0 @@
-def main():
-    print("hello world")
-    print("can you see me?")
-    print("what about now?")
-    print("hello")
-    print("here is more code. we love coding!")
-    mel
