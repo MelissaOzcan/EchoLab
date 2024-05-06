db022af6b31ec36a70fea3904ef8d563443a8262
sabah-naveed <90809996+sabah-naveed@users.noreply.github.com>
2024-05-06 15:44:39 -0400

    remove video

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -4,18 +4,18 @@ import { useEffect, useRef } from "react";
 export const VideoPlayer = ({ user, username }) => {
   const ref = useRef();
   useEffect(() => {
-    user.videoTrack.play(ref.current);
+    // user.videoTrack.play(ref.current);
     console.log("playing video track");
   }, []);
 
   return (
     <div>
-      Uid: {user.uid}
+      Uid: {user.uid} is in VC
       {/* Username: {username} */}
-      <div
+      {/* <div
         ref={ref}
         style={{ width: '200px', height: '200px' }}
-      ></div>
+      ></div> */}
     </div>
   );
 };
