e5eb0cc511c59ac2bf1699f3b5d54a4f8b4a00fe
Melissa Ozcan <ozcanmelissa@gmail.com>
2024-05-07 21:41:15 -0400

    delete user functionality

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@
 
 import compileRoutes from './compile.js';
 import deleteRoomRoutes from './deleteroom.js';
+import deleteUserRoutes from './deleteuser.js';
 import editorRoutes from './editor.js';
 import homeRoutes from './home.js';
 import loginRoutes from './login.js';
@@ -19,6 +20,7 @@ const constructorMethod = (app) => {
     app.use('/home', homeRoutes);
     app.use('/compile', compileRoutes);
     app.use('/deleteroom', deleteRoomRoutes);
+    app.use('/deleteuser', deleteUserRoutes);
     app.use('*', (_, res) => {
         res.redirect('/login');
     });
